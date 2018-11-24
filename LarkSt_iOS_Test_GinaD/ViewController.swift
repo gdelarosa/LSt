1509e1bb66b74bcf6e1362b12398954c15e07986
Gina <g26delarosa@gmail.com>
2018-11-24 14:19:05 -0800

    Initial Commit

---
+++ LarkSt_iOS_Test_GinaD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LarkSt_iOS_Test_GinaD
+//
+//  Created by Gina De La Rosa on 11/24/18.
+//  Copyright © 2018 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
